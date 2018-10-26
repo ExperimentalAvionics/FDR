fb5894d9ad664b9132c75e788a658acc2c8544a1
Oleg <oleg@experimentalavionics.com>
2018-10-26 16:59:48 +1100

    intial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# FDR
+
+This Python script reads data captured by the CAN_Listener and writes it into a flat file (.csv) into the ~/fdr_data/ folder at preconfigured intervals
+
+Prerequisites:
+CAN_Listeners script running
+
---
+++ fdr.py
@@ -0,0 +1,43 @@
+import os
+import sqlite3
+import time
+from time import sleep
+from datetime import datetime
+
+filename = "/home/pi/fdr_data/fdr_"
+logperiod = 5
+theline = ""
+
+now = datetime.now()
+filename += str(now)[:10]
+filename += ".csv"
+
+#get the database ready
+memdb = sqlite3.connect('/memdb/memdb.db')
+cursor = memdb.cursor()
+
+
+file = open(filename, "a")
+i=0
+if os.stat(filename).st_size == 0:
+	theline = "Time"
+	cursor.execute("SELECT * FROM messages ORDER BY id")
+	rows = cursor.fetchall()
+	for row in rows:
+		theline += "," + str(row[2])
+	theline += "\n"
+	file.write(theline)
+while True:
+	i=i+1
+	now = datetime.now()
+	theline = str(now)
+	cursor.execute("SELECT * FROM messages ORDER BY id")
+	rows = cursor.fetchall()
+	for row in rows:
+		theline += "," + str(row[3])
+	theline += "\n"
+#	print(theline)
+	file.write(theline)
+	file.flush()
+	time.sleep(logperiod)
+file.close()
